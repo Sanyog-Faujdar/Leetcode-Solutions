a346009a401e7497f45fe6407b13148a60b7b2eb
Sanyog-Faujdar <sanyogfaujdar@gmail.com>
2026-03-23 08:26:32 +0530

    Added Container With Most Water solution with greedy two-pointer approach

---
+++ Two Pointers/container-with-most-water.py
@@ -0,0 +1,22 @@
+# Problem : Container with most Water
+# link : https://leetcode.com/problems/container-with-most-water/submissions/1888727753
+# Approach : Greedy (move pointer with smaller height to maximize area)
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+# Insight : Area depends on min height, so move smaller pointer
+
+class Solution(object):
+    def maxArea(self, height):
+        
+        capacity =0
+        i= 0 
+        j=len(height)-1
+        while i<j:
+            distance = j-i
+            area = min(height[j],height[i]) *distance
+            capacity = max(capacity,area)
+            if height[i]>height[j]:
+                j-=1
+            else :
+                i+=1
+        return capacity
